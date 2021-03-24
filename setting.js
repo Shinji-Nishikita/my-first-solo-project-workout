7157c1d3d4fb3163f574bc48652ea63bbf55619e
Shinji-Nishikita <pizattu@gmail.com>
2021-03-24 20:52:43 +0900

    setting.js追加

--- setting.js
+++ setting.js
@@ -2,18 +2,17 @@ const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
 });
 
-client.connect();
+// client.connect();
 
-client.query(
-  "CREATE TABLE mydata(id SERIAL PRIMALY KEY, name text, age int, weight int, height int);",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
+// client.query(
+//   "CREATE TABLE mydata(id SERIAL PRIMALY KEY, name text, age int, weight int, height int);",
+//   (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//       console.log(JSON.stringify(row));
+//     }
+//     client.end();
+//   }
+// );
