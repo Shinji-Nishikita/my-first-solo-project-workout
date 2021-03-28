f752a2e122bfa8f21ec0d1e6d4e18cc3e9686372
Shinji-Nishikita <pizattu@gmail.com>
2021-03-28 23:08:44 +0900

    fixed

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "macforpizza",
-  host: "postgresql-cylindrical-79933",
+  host: "localhost",
   port: "5432",
   database: "workoutdata",
 });
