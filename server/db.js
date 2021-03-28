f27d279d7bc1c634d7eadd7d1f1c3158bd61f1d9
Shinji-Nishikita <pizattu@gmail.com>
2021-03-28 12:24:04 +0900

    fixed

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOSTNAME,
-  port: 5432,
-  database: process.env.DB_DATABASE,
+  user: "macforpizza",
+  host: "localhost",
+  port: "5432",
+  database: "workoutdata",
 });
 
 module.exports = pool;
